8302fac3d9cf661d27db2ec8d014d87fdbeb3fb2
dqyang <yangdq@hotmail.com>
2021-08-05 18:39:37 -0500

    Atos Demo

--- src/main/java/com/inventsolv/springsecurityjpajwt/dao/AccountRepository.java
+++ src/main/java/com/inventsolv/springsecurityjpajwt/dao/AccountRepository.java
@@ -3,9 +3,11 @@ package com.inventsolv.springsecurityjpajwt.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.inventsolv.springsecurityjpajwt.models.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+	@Query(value = "SELECT * FROM ACCOUNT WHERE ACCOUNT_NUMBER = ?1 AND ACTIVE = 1", nativeQuery = true)
     Optional<Account> findByAccountNumber(String accountNumber);
 }
--- src/main/java/com/inventsolv/springsecurityjpajwt/dao/UserRepository.java
+++ src/main/java/com/inventsolv/springsecurityjpajwt/dao/UserRepository.java
@@ -3,11 +3,14 @@ package com.inventsolv.springsecurityjpajwt.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.inventsolv.springsecurityjpajwt.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+	@Query(value = "SELECT * FROM CUSTOMER WHERE USER_NAME = ?1 AND ACTIVE = 1", nativeQuery = true)
     Optional<User> findByUserName(String userName);
     
+	@Query(value = "SELECT * FROM CUSTOMER WHERE CUSTOMER_NUMBER = ?1 AND ACTIVE = 1", nativeQuery = true)
     Optional<User> findByCustomerNumber(String customerNumber);
 }
